41d906ea6b5958315672c6c08c9331d23b0b2c9e
Grant Stampfli <mail@gstampfli.com>
2018-06-21 12:25:51 -0700

    added firebase fn

---
+++ src/views/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'Contact',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Slack.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'Slack',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
